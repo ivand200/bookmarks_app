8b8b7b6b4ad7f4a66547c6ac66d31e85c49b6ead
Ivan Dubograi <ivandubograi@Ivans-MacBook-Air.local>
2026-03-11 19:39:36 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  let { data } = $props();
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<form method="POST" action="?/{data.user ? 'signOut' : 'signIn'}">
+  <button type="submit">Sign {data.user ? "out" : "in"}</button>
+</form>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let data;
+</script>
+
+<h1>Test</h1>
+{#if data.user}
+  <ul>
+    {#each Object.entries(data.user) as [key, value]}
+      <li>{key}: {value}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>Not authenticated</p>
+{/if}
